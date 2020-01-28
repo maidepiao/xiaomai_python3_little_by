a829fc068a23c8002fd63e7040ce09080a03b898
huhongxing <18510255246@163.com>
2020-01-28 20:59:20 +0800

    整理目录

--- com/star/网络编程/UDPClient.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.sendto('udp client'.encode(),('127.0.0.1',9000))
-print(client.recv(1024).decode())
-client.close()
--- com/star/网络编程/UDPServer.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server.bind(('127.0.0.1',9000))
-msg,addr = server.recvfrom(1024)
-print(msg.decode())
-server.sendto('udp server'.encode(),addr)
-server.close()
